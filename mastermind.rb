9e9fa0e8bd3de8a377d2c2a534ac6c9784cbf455
Kate Fulton <katesuzannefulton@gmail.com>
2014-09-12 08:37:11 -0700

    Checks for answer and guesses matching colors- not specific to index position.

--- mastermind.rb
+++ mastermind.rb
@@ -1,7 +1,9 @@
 #TO DO
 #DONEcreate a random combination of 4 colors from 8 colors
+#DONEallow for guesses
+
+
 #create a board/reprint board
-#allow for guesses
 #respond to guesses - number of correct colors and number of colors in correct order
 #a way to quit the game
 #an end to the game
@@ -32,10 +34,11 @@ class MasterMind
   end
 
   def get_guess
+    puts @answer.inspect
+
     if @guess.length <= 3
       puts "Choose a color."
       @user_guess = gets.chomp.downcase
-      puts @guess.length.inspect
       guess_to_array
     else
       return_answers
@@ -43,21 +46,20 @@ class MasterMind
   end
 
   def return_answers
-    puts "HELLO!"
     if @answer == @guess
       puts "You win!"
     end
 
-    if @answer.include?(@guess)
-      puts "true"
-    end
-    #right_color = 0
+    right_color = 0
 
     @answer.each_with_index do |color, index|
-      if @guess == color
-        puts "true"
+      if @guess.include?(color)
+        right_color += 1
       end
     end
+
+    puts "You have #{right_color} correct colors."
+
     # @answer.each_with_index do |color, index|
     #   if @guess[index] == color[index]
     #     right_order += 1
